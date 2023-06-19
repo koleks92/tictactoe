c427d0a0c966ec2ea3221bc6b23e560b4b76bd16
koleks92 <koleks92@gmail.com>
2023-06-19 18:12:11 +0200

    Func result, winner, terminal, utlility

--- tictactoe.py
+++ tictactoe.py
@@ -2,7 +2,7 @@
 Tic Tac Toe Player
 """
 
-import math
+import math, copy
 
 X = "X"
 O = "O"
@@ -57,7 +57,7 @@ def actions(board):
     actions = set()
     board_state = board()
 
-    # Loop with enumarte thourgh board, if 'EMPTY' add to actions
+    # Loop with enumearte through board, if 'EMPTY' add to actions
     for i, j in enumerate(board_state):
         for k, l in enumerate(j):
             if l == EMPTY:
@@ -73,30 +73,72 @@ def result(board, action):
     """
     if action not in actions(board):
         raise ValueError
-    
-    
+    else:
+        # Deepcopy of the board
+        c_board = copy.deepcopy(board)
+        c_board_state = c_board()
+        
+        # Get action indexes
+        (i, j) = action
 
+        # Get the mark and set on the board
+        mark = player(board)
+        c_board_state[i][j] = mark
+
+        return c_board_state
+        
 
 def winner(board):
     """
     Returns the winner of the game, if there is one.
     """
-    raise NotImplementedError
+
+    board_state = board()
+
+    # Check rows
+    for row in board_state:
+        if row[0] == row[1] == row[2] != EMPTY:
+            return row[0]
+        
+    # Check columns
+    for column in range(3):
+        if board_state[0][column] == board_state[1][column] == board_state[2][column] != EMPTY:
+            return board_state[0][column]
+        
+    # Check diagonals
+    if board_state[0][0] == board_state[1][1] == board_state[2][2] != EMPTY:
+        return board_state[0][0]
+    if board_state[0][2] == board_state[1][1] == board_state[2][0] != EMPTY:
+        return board_state[0][2]
+    
+    # If no winner
+    return None
+ 
 
 
 def terminal(board):
     """
     Returns True if game is over, False otherwise.
     """
-    raise NotImplementedError
+
+    if len(actions(board)) == 0 or winner(board) == X or winner(board) == O:
+        return True
+    else:
+        return False
 
 
 def utility(board):
     """
     Returns 1 if X has won the game, -1 if O has won, 0 otherwise.
     """
-    raise NotImplementedError
-
+    if terminal(board) == True:
+        if winner(board) == X:
+            return 1
+        elif winner(board) == O:
+            return -1
+        else:
+            return 0
+    
 
 def minimax(board):
     """
