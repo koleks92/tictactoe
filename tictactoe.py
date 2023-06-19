13a1d4860b814c71ee938dbf27a78e3041ca463f
koleks92 <koleks92@gmail.com>
2023-06-19 11:03:43 +0200

    Functions player, actions

--- tictactoe.py
+++ tictactoe.py
@@ -22,21 +22,59 @@ def player(board):
     """
     Returns player who has the next turn on a board.
     """
-    raise NotImplementedError
+    # Create varable to count the turn
+    xs = 0
+    os = 0
+    emptys = 0
+
+    # Initiate the board
+    board_state = board()
+
+    # Loop through each line, and each mark
+    for line in board_state:
+        for m in line:
+            if m == X:
+                xs += 1
+            if m == O:
+                os += 1
+            if m == EMPTY:
+                emptys += 1
+
+    if emptys == 9:
+        return X
+    if xs > os:
+        return O
+    if os == xs:
+        return X
 
 
 def actions(board):
     """
     Returns set of all possible actions (i, j) available on the board.
     """
-    raise NotImplementedError
+
+    # Create state and initiate board
+    actions = set()
+    board_state = board()
+
+    # Loop with enumarte thourgh board, if 'EMPTY' add to actions
+    for i, j in enumerate(board_state):
+        for k, l in enumerate(j):
+            if l == EMPTY:
+                actions.add((i, k))
+
+    return actions
+                
 
 
 def result(board, action):
     """
     Returns the board that results from making move (i, j) on the board.
     """
-    raise NotImplementedError
+    if action not in actions(board):
+        raise ValueError
+    
+    
 
 
 def winner(board):
